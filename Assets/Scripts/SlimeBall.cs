1c7530485dd57e1ea8102a48bb187dde2fa5c3cb
ShaneDev08 <46009041+ShaneDev08@users.noreply.github.com>
2021-07-12 18:09:43 +0100

    ScoreandFlag System

--- Assets/Scripts/SlimeBall.cs
+++ Assets/Scripts/SlimeBall.cs
@@ -6,15 +6,28 @@ using Cinemachine;
 public class SlimeBall : MonoBehaviour
 {
     private CinemachineTargetGroup target1;
-
+    public GameObject flag;
+    private bool hasStopedMoving = false;
+    int score;
     private void Start()
     {
         target1 = GameObject.Find("TargetGroup").GetComponent<CinemachineTargetGroup>();
     }
     private void Update()
     {
+       
+        if((int)transform.position.x > score)
+        score = (int)transform.position.x;
 
-        Invoke("KillSlime", 5);
+        UIManager.instance.UpdateScoreText(score);
+
+        if(GetComponent<Rigidbody2D>().IsSleeping() && !hasStopedMoving)
+        {
+            hasStopedMoving = true;
+            Instantiate(flag, transform.position, flag.transform.rotation);
+            KillSlime();
+            
+        }
 
     }
 
@@ -22,5 +35,6 @@ public class SlimeBall : MonoBehaviour
     private void KillSlime()
     {
         target1.RemoveMember(this.transform);
+        Destroy(this.gameObject);
     }
 }

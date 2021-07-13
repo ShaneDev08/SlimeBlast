f4c7848dbd13ddc375641fdc3a4d90a2299c9331
JSmithPollard <38458856+JSmithPollard@users.noreply.github.com>
2021-07-13 11:49:04 +0100

    Added Scriptable Object for Slime Character

---
+++ Assets/Scripts/ScriptableObject/SlimeStats.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName ="SlimeCharacter")]
+public class SlimeStats : ScriptableObject {
+    
+
+
+    [Header("Slime Main Stats")]
+    public float slimeHealth;
+    public float slimeBounciness;
+    public float slimeFriction;
+
+    [Header("Slime Secondary Stats")]
+    public string slimeName;
+
+    [Header("SlimeObjectReferences")]
+    public GameObject slimeObject;
+    public Sprite slimeSprite;
+    public Color slimeBackground;
+    public AudioClip slimeNoise;
+
+}

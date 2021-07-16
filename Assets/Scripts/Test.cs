7d805392062751540e01cc4539671f5481eacc28
ShaneDev08 <46009041+ShaneDev08@users.noreply.github.com>
2021-07-16 10:45:02 +0100

    Floor(maybe)
    Floor

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public int test;
+    private SpriteRenderer sprite;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer> ();
+
+        if(test == 1)
+        {
+            transform.position = new Vector3(484.85f, transform.position.y, transform.position.z);
+        }
+
+        else if (test == 2)
+        {
+            transform.position = new Vector3(984.7f, transform.position.y, transform.position.z);
+        }
+        CreateLevel();
     }
 
     // Update is called once per frame
-    void Update()
+   void CreateLevel()
     {
-        
+        sprite.size = new Vector2(500, 9.373164f);
+        if(test == 2)
+        {
+            sprite.size = new Vector2(500, 7.4f);
+        }
     }
 }

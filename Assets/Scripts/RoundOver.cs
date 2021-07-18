24678b3eee8dc4ee6e85202ab852745f9137ea01
JSmithPollard <38458856+JSmithPollard@users.noreply.github.com>
2021-07-18 17:20:32 +0100

    Updated RoundOverScreen Buttons

--- Assets/Scripts/RoundOver.cs
+++ Assets/Scripts/RoundOver.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoundOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    
+    public void HomeScreen(){
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void RetryButton(){
+        SceneManager.LoadScene("DevScene 1");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ShopScreen(){
+        SceneManager.LoadScene("Shop");
     }
+
 }

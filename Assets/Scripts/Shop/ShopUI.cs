ac45d36837673cb98ff687f8786b4fe7dc130321
JSmithPollard <38458856+JSmithPollard@users.noreply.github.com>
2021-07-19 19:51:17 +0100

    Updated the Shop Menu
    Added reference to PlayerManager for Money Values
    Added transitions between upgrade interfaces

--- Assets/Scripts/Shop/ShopUI.cs
+++ Assets/Scripts/Shop/ShopUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ShopUI : MonoBehaviour
 {
@@ -14,33 +15,53 @@ public class ShopUI : MonoBehaviour
     public AudioClip onButtonClick;
     public AudioSource audio;
     public float volume;
-    public GameObject cannonUpgradeContainer;
-    public GameObject slimeUpgradeContainer;
-    public GameObject worldUpgradeContainer;
-
+    public Transform cannonUpgradeContainer;
+    public CanvasGroup backgroundCannon;
+    public Transform slimeUpgradeContainer;
+    public CanvasGroup backgroundSlime;
+    public Transform worldUpgradeContainer;
+    public CanvasGroup backgroundWorld;
+    public TextMeshProUGUI moneyText;
+    [Header("ObjectReferences")]
+    PlayerManager playerManager;
     [Header("TweenValues")]
     [SerializeField] private float tweenTime;
 
     public void Start()
     {
         audio = GameObject.Find("ShopCanvas").GetComponent<AudioSource>();
+        playerManager = GetComponent<PlayerManager>();
+        
+    }
+
+    private void Update() {
+        updateMoneyValue();
     }
 
     public void onClickCannonUpgrade()
     {
         PlayButtonClick();
         LeanTween.scale(cannonUpgradeButton, Vector3.one * 2, tweenTime).setEasePunch();
+        CannonMoveContainer();
+        slimeUpgradeContainer.LeanMoveLocalY(-Screen.height,tweenTime);
+        worldUpgradeContainer.LeanMoveLocalY(-Screen.height,tweenTime);
     }
 
     public void onClickSlimeUpgrade()
     {
         PlayButtonClick();
         LeanTween.scale(slimeUpgradeButton, Vector3.one * 2, tweenTime).setEasePunch();
+        SlimeMoveContainer();
+        cannonUpgradeContainer.LeanMoveLocalY(-Screen.height,tweenTime);
+        worldUpgradeContainer.LeanMoveLocalY(-Screen.height,tweenTime);
     }
     public void onClickWorldUpgrade()
     {
         PlayButtonClick();
         LeanTween.scale(worldUpgradeButton, Vector3.one * 2, tweenTime).setEasePunch();
+        WorldMoveContainer();
+        slimeUpgradeContainer.LeanMoveLocalY(-Screen.height,tweenTime);
+        cannonUpgradeContainer.LeanMoveLocalY(-Screen.height,tweenTime);
     }
 
     public void PlayButtonClick()
@@ -58,4 +79,27 @@ public class ShopUI : MonoBehaviour
         SceneManager.LoadScene("DevScene 1");
     }
     
+    public void CannonMoveContainer()
+    {
+      backgroundCannon.alpha = 0;
+      backgroundCannon.LeanAlpha(1, tweenTime);
+
+      cannonUpgradeContainer.localPosition = new Vector2 (0, -Screen.height);
+      cannonUpgradeContainer.LeanMoveLocalY(-65,tweenTime).setEaseOutExpo();
+    }
+    public void SlimeMoveContainer()
+    {
+      slimeUpgradeContainer.localPosition = new Vector2 (0, -Screen.height);
+      slimeUpgradeContainer.LeanMoveLocalY(-65,tweenTime).setEaseOutExpo();
+    }
+    public void WorldMoveContainer()
+    {
+      worldUpgradeContainer.localPosition = new Vector2 (0, -Screen.height);
+      worldUpgradeContainer.LeanMoveLocalY(-65,tweenTime).setEaseOutExpo();
+    }
+
+    public void updateMoneyValue()
+    {
+        moneyText.text = "Money:" + PlayerManager.money.ToString();
+    }
 }

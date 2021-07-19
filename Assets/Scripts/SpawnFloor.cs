e69863ba2d8033fb049da6d517bb999fa3c66e9f
ShaneDev08 <46009041+ShaneDev08@users.noreply.github.com>
2021-07-19 11:40:08 +0100

    ExtraChangeToFloor

--- Assets/Scripts/SpawnFloor.cs
+++ Assets/Scripts/SpawnFloor.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 public class SpawnFloor : MonoBehaviour
 {
     private SpriteRenderer sprite;
-    public Floors floor;
+    //public Floors floor;
+
+    public GameObject[] floors;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        sprite = GetComponent<SpriteRenderer>();
-
-        if (floor.type == Floors.Type.Ice)
+        
+        foreach (GameObject floor in floors)
         {
-            transform.position = new Vector3(484.85f, transform.position.y, transform.position.z);
-            sprite.size = new Vector2(500, 11.78f);
-        }
+            sprite = floor.GetComponent<SpriteRenderer>();
+            FloorHolder gameFloor = floor.GetComponent<FloorHolder>();
+            if (gameFloor.floor.type == Floors.Type.Ice)
+            {
+                floor.transform.position = new Vector3(floors[0].transform.position.x + 500 - 10, floor.transform.position.y, floor.transform.position.z);
+                //floor.transform.position = new Vector3(484.85f, floor.transform.position.y, floor.transform.position.z);
+                sprite.size = new Vector2(500, 11.78f);
+            }
 
-        else if (floor.type == Floors.Type.Sand)
-        {
-            transform.position = new Vector3(984.7f, transform.position.y, transform.position.z);
+            else if (gameFloor.floor.type == Floors.Type.Sand)
+            {
+                floor.transform.position = new Vector3(floors[1].transform.position.x + 500, floor.transform.position.y, floor.transform.position.z);
+            }
+            CreateLevel(gameFloor);
         }
-        CreateLevel();
+        
+
+        
+
+       
+        
     }
 
     // Update is called once per frame
@@ -34,38 +47,38 @@ public class SpawnFloor : MonoBehaviour
 
 
 
-    void CreateLevel()
+    void CreateLevel( FloorHolder gameFloor)
     {
         sprite.size = new Vector2(500, 9.373164f);
 
-        if (floor.type == Floors.Type.Grass)
+        if (gameFloor.floor.type == Floors.Type.Grass)
         {
             for (int i = 0; i < 25; i++)
             {
-                int randomNumber = Random.RandomRange(0, floor.objects.Length);
-                Instantiate(floor.objects[randomNumber], new Vector2(Random.RandomRange(0, 470), floor.objects[randomNumber].transform.position.y), transform.rotation);
+                int randomNumber = Random.RandomRange(0, gameFloor.floor.objects.Length);
+                Instantiate(gameFloor.floor.objects[randomNumber], new Vector2(Random.RandomRange(5, 470), gameFloor.floor.objects[randomNumber].transform.position.y), transform.rotation);
             }
         }
 
-        if (floor.type == Floors.Type.Ice)
+        if (gameFloor.floor.type == Floors.Type.Ice)
         {
             for (int i = 0; i < 25; i++)
             {
-                int randomNumber = Random.RandomRange(0, floor.objects.Length);
-                Instantiate(floor.objects[randomNumber], new Vector2(Random.RandomRange(490, 960), floor.objects[randomNumber].transform.position.y), transform.rotation);
+                int randomNumber = Random.RandomRange(0, gameFloor.floor.objects.Length);
+                Instantiate(gameFloor.floor.objects[randomNumber], new Vector2(Random.RandomRange(490, 960), gameFloor.floor.objects[randomNumber].transform.position.y), transform.rotation);
             }
         }
 
 
 
-        if (floor.type == Floors.Type.Sand)
+        if (gameFloor.floor.type == Floors.Type.Sand)
         {
             sprite.size = new Vector2(500, 7.4f);
 
             for (int i = 0; i < 25; i++)
             {
-                int randomNumber = Random.RandomRange(0, floor.objects.Length);
-                Instantiate(floor.objects[randomNumber], new Vector2(Random.RandomRange(980, 1460), floor.objects[randomNumber].transform.position.y), transform.rotation);
+                int randomNumber = Random.RandomRange(0, gameFloor.floor.objects.Length);
+                Instantiate(gameFloor.floor.objects[randomNumber], new Vector2(Random.RandomRange(980, 1460), gameFloor.floor.objects[randomNumber].transform.position.y), transform.rotation);
             }
         }
     }

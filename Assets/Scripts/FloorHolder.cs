afb8dc778270d862310f20b7634a2d87104c6eb3
ShaneDev08 <46009041+ShaneDev08@users.noreply.github.com>
2021-07-22 17:32:41 +0100

    Music

--- Assets/Scripts/FloorHolder.cs
+++ Assets/Scripts/FloorHolder.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 public class FloorHolder : MonoBehaviour
 {
     public Floors floor;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && PlayerManager.instance.slimeBall.GetComponent<SlimeBall>().slimeStats.multipleRbs)
+        {
+            collision.gameObject.GetComponent<JellySpriteReferencePoint>().ParentJellySprite.GetComponent<SlimeBall>().PlaySound();
+        }
+        else
+        {
+            collision.gameObject.GetComponent<SlimeBall>().PlaySound();
+        }
+
+    }
 }

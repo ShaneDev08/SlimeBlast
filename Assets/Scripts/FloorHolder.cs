81b6ce4ccf3d58d64c368d1f48c274a99b5273f2
ShaneDev08 <46009041+ShaneDev08@users.noreply.github.com>
2021-07-24 15:42:27 +0100

    MoreHealthSystem

--- Assets/Scripts/FloorHolder.cs
+++ Assets/Scripts/FloorHolder.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class FloorHolder : MonoBehaviour
 {
+
+    [Header("Damage Amount")]
+    [SerializeField] private int damage;
     public Floors floor;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && PlayerManager.instance.slimeBall.GetComponent<SlimeBall>().slimeStats.multipleRbs)
         {
-            collision.gameObject.GetComponent<JellySpriteReferencePoint>().ParentJellySprite.GetComponent<SlimeBall>().TakeDamage(30);
+            collision.gameObject.GetComponent<JellySpriteReferencePoint>().ParentJellySprite.GetComponent<SlimeBall>().TakeDamage(damage);
             collision.gameObject.GetComponent<JellySpriteReferencePoint>().ParentJellySprite.GetComponent<SlimeBall>().PlaySound();
         }
         else

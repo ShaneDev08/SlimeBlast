0e08e18249379915cd466eb90a4b57e9703bd7d1
ShaneDev08 <46009041+ShaneDev08@users.noreply.github.com>
2021-08-02 11:33:04 +0100

    CloudsAndPlane

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public class Cloud : MonoBehaviour
 {
 
-    private Transform startPos;
+    private Vector3  startPos;
+    private GameObject slimeSpawnd;
+    public ObjectPool pool;
+    private bool hasSpawned = false;
 
     private void Start()
     {
-        startPos = transform;
-        Debug.Log(startPos.transform.position.x);
-        Debug.Log(transform.position.x);
+        startPos = this.transform.position;
+
+        
+
     }
     private void Update()
     {
-        //this.transform.position += new Vector3(-1 * Time.deltaTime, 0, 0);
-        
-        if(transform.position.x <= -10)
+        if (!hasSpawned)
         {
-          
-            transform.position =  new Vector3(startPos.position.x,startPos.position.y,startPos.position.z);
+            
+            slimeSpawnd = pool.GetCurrentActiveObject();
+            if(slimeSpawnd != null)
+            {
+                hasSpawned = true;
+            }
+        }
+
+        this.transform.position += new Vector3(-1 * Time.deltaTime, 0, 0);
+
+        if (slimeSpawnd != null)
+        {
+            if (transform.position.x <= slimeSpawnd.transform.position.x - 25)
+            {
+                Debug.Log("Plane has gvone past");
+                this.gameObject.transform.position = new Vector2(slimeSpawnd.transform.position.x + 50, transform.position.y);
+            }
         }
            
         

500313122143f0fc4794a07f025b1177f09c1de9
ShaneDev08 <46009041+ShaneDev08@users.noreply.github.com>
2021-08-12 13:40:09 +0100

    CannonUpgrade

---
+++ Assets/Scripts/UpgradeScripts/CannonPowerUpgrade.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[Serializable]
+public class CannonPowerUpgrade
+{
+    [Header("Power Upgrade Stats")]
+    public int upgradeAmount = 10;
+    public int currentUpgrade;
+    public int upgradeValue;
+    public int powerModifer;
+    public int upgradeChangeValue;
+
+    private bool CanBuy()
+    {
+        if (PlayerManager.money >= upgradeValue  && currentUpgrade < upgradeAmount)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    public void BuyUpgrade()
+    {
+        if (CanBuy())
+        {
+            powerModifer += upgradeChangeValue;
+            PlayerManager.money -= upgradeValue;
+            currentUpgrade += 1;
+            upgradeValue += upgradeValue / 100 * 15;
+        }
+    }
+
+}

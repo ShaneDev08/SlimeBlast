a3cc0ec6c96d7873f16033d583c2cdc9fd3c5534
JSmithPollard <38458856+JSmithPollard@users.noreply.github.com>
2021-09-20 13:12:43 +0100

    SettingsScreenV1

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Button soundButton;
+    public Sprite soundUnMute;
+    public Sprite soundMute;
+    public bool isMuted;
+    public bool isConnectedToPlayServices;
+
+    public void SoundControl()
+    {
+        isMuted = !isMuted;
+        if(isMuted)
+        {
+            soundButton.GetComponent<Image>().sprite = soundUnMute;
+        } 
+        else 
+        {
+            soundButton.GetComponent<Image>().sprite = soundMute;
+        }
+
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void StartSignIn()
+        {
+            PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce,(result)=>
+            {
+                switch(result)
+                {
+                    case SignInStatus.Success:
+                    isConnectedToPlayServices = true;
+                    break;
+                    default:
+                    isConnectedToPlayServices = false;
+                    break;
+                }
+            });
+
+}
+}
